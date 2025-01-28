4e6561dd6e19cd51198bef169d7766a634c40395
이재민 <User@ijaemin-ui-MacBook-Air.local>
2025-01-28 19:28:58 +0900

    자바 프로젝트 첫 커밋

---
+++ src/operator/Operator4.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Operator4 {
+
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3; //(2 * 2) + (3 * 3)
+        System.out.println("sum3 = " + sum3);
+        int sum4 = (2 * 2) + (3 * 3); //sum3과 같다
+        System.out.println("sum4 = " + sum4);
+        // 똑같기 떄문에 가로를 넣어주는 것이 가독성이 더 좋음
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVar = 1000000000L;
+        System.out.println("longVar = " + longVar);
+        
+        boolean boolenVar = true;
+        System.out.println("boolenVar = " + boolenVar);
+    }
+}
