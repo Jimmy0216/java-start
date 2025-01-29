1fbba5a08cda37b75eb67c3bde2b7a7088c1f1b5
jimmy0216 <maximin910216@gmail.com>
2025-01-29 15:56:56 +0900

    전역변수, 지역변수 강의

--- src/loop/While2_3.java
+++ src/loop/While2_3.java
@@ -4,8 +4,8 @@ public class While2_3 {
 
     public static void main(String[] args) {
         int sum = 0;
-        int i = 50;
-        int endNum = 100;
+        int i = 1;
+        int endNum = 3;
 
         while (i <= endNum){
             sum = sum + i;
