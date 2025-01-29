5e44b3c6a197cb842b8420fa6f328c924ddfd05c
jimmy0216 <maximin910216@gmail.com>
2025-01-29 13:25:36 +0900

    반복문 while문, for문

---
+++ src/loop/ex/WhileEx1.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class WhileEx1 {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <= 10){
+            System.out.println("현재 숫자는: " + count);
+            count++;
+        }
+//        int count = 0;
+//        while(count < 10){
+//            count = count + 1;
+//            System.out.println("현재 숫자는: " + count);
+    }
+}
